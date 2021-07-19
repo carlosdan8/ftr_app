afd2b4f35cb37d949921b87de4b9775efb165755
Dan <icarlosdan8@gmail.com>
2021-07-19 10:09:58 -0500

    Second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      'Hola Mundo',
-      textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: 'miApp',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Hola'),
+      ),
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('Cliked');
+          }),
     ),
   ));
 }
