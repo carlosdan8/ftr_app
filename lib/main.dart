9a1c19a8cbf0076f70d6c62780ce2f3788e6055f
Dan <icarlosdan8@gmail.com>
2021-07-19 12:42:37 -0500

    Third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'miApp',
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Hola'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'miApp',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hola'),
+        ),
+        body: Column(
+          children: <Tarea>[Tarea('Primera tarea'), Tarea('Segunda tarea')],
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              print('Cliked');
+            }),
       ),
-      body: Center(
-        child: Text('Hola Mundo'),
-      ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('Cliked');
-          }),
-    ),
-  ));
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class Tarea extends StatelessWidget {
+  String tarea = '';
+
+  Tarea(this.tarea);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Text(tarea));
+    throw UnimplementedError();
+  }
 }
